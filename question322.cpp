8e6112b85e00fd8121285bff8f767f02e265fa5b
Electromagnetism-dog-technology <346322521@qq.com>
2021-06-02 17:05:34 +0800

    62

---
+++ question322.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+//1¡¢BFS
+class Solution1 {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>>res;
+		if (!root)return res;
+		queue<TreeNode*>q;
+		q.push(root);
+		while (!q.empty())
+		{
+			vector<int>vec;
+			int len = q.size();
+			for (int i = 0; i < len; i++)
+			{
+				auto t = q.front();
+				q.pop();
+				vec.push_back(t->val);
+				if (t->left)q.push(t->left);
+				if (t->right)q.push(t->right);
+			}
+
+			res.push_back(vec);
+		}
+
+		return res;
+	}
+
+};
+
+//2¡¢DFS
+class Solution2 {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> res;
+		dfs(root, res, 0);
+		return res;
+	}
+	void dfs(TreeNode* root, vector<vector<int>>& res, int level)
+	{
+		if (!root) return;
+		if (level >= res.size()) res.push_back(vector<int>());
+		res[level].push_back(root->val);
+		dfs(root->left, res, level + 1);
+		dfs(root->right, res, level + 1);
+	}
+};
+
+
+
+int main()
+{
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
--- question33.cpp
+++ question33.cpp
@@ -1,71 +1,41 @@
 #include<iostream>
-#include<string>
 #include<vector>
-#include<queue>
+
 
 using namespace std;
 
 
-//Definition for a binary tree node.
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-//1¡¢BFS
-class Solution1 {
+class Solution {
 public:
-	vector<vector<int>> levelOrder(TreeNode* root) {
-		vector<vector<int>>res;
-		if (!root)return res;
-		queue<TreeNode*>q;
-		q.push(root);
-		while (!q.empty())
-		{
-			vector<int>vec;
-			int len = q.size();
-			for (int i = 0; i < len; i++)
-			{
-				auto t = q.front();
-				q.pop();
-				vec.push_back(t->val);
-				if (t->left)q.push(t->left);
-				if (t->right)q.push(t->right);
-			}
+	bool verifyPostorder(vector<int>& postorder) {
+		return dfs(0, postorder.size()-1,postorder);
+	}
 
-			res.push_back(vec);
+	bool dfs(int left, int right, vector<int>& postorder)
+	{
+		if (left >= right) return true;//ÍË³öÌõ¼þ
+		int k=left;
+		int root = postorder[right];
+		while (k < right && postorder[k] < root) k++;
+
+		for (int i = k; i < right; i++)
+		{
+			if (postorder[i] < root)
+				return false;
 		}
 
-		return res;
-	}
+		return dfs(left, k - 1, postorder) && dfs(k, right - 1, postorder);
 
-};
-
-//2¡¢DFS
-class Solution2 {
-public:
-	vector<vector<int>> levelOrder(TreeNode* root) {
-		vector<vector<int>> res;
-		dfs(root, res, 0);
-		return res;
-	}
-	void dfs(TreeNode* root, vector<vector<int>>& res, int level)
-	{
-		if (!root) return;
-		if (level >= res.size()) res.push_back(vector<int>());
-		res[level].push_back(root->val);
-		dfs(root->left, res, level + 1);
-		dfs(root->right, res, level + 1);
 	}
 };
-
 
 
 int main()
 {
-
+	vector<int>postorder = { 4, 8, 6, 12, 16, 14, 10 };
+	Solution newsolution;
+	bool res = newsolution.verifyPostorder(postorder);
+	cout << res << endl;
 	system("pause");
 	return EXIT_SUCCESS;
 }
